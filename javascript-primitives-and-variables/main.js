77fac4d0bc1aaedc3c3f1791bcf6e730e8926e17
Anna Aksenov <annaaksenov8@gmail.com>
2023-03-21 03:38:02 +0000

    Completed js-primatives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+const fullName = 'Anna Aksenov';
+console.log('Full name:', fullName);
+console.log(typeof fullName);
+const isCool = true;
+console.log('Is cool?:', isCool);
+console.log(typeof isCool);
+const totalPets = 2;
+console.log('Total pets:', totalPets);
+console.log(typeof totalPets);
+const unknown = null;
+console.log('Log unknown:', unknown);
+console.log(typeof unknown);
+let never;
+console.log('Log never:', never);
+console.log(typeof never);
