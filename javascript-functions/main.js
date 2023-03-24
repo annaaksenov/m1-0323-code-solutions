20e825b7a4dda0aa7bd1d4405cba62a9ea354c0b
Anna Aksenov <annaaksenov8@gmail.com>
2023-03-24 16:36:51 +0000

    Started js-functions

---
+++ javascript-functions/main.js
@@ -0,0 +1,10 @@
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+console.log(convertMinutesToSeconds(5));
+
+function greet(name) {
+  return 'Hey, ' + name + '!';
+}
+const toGreet = greet('Beavis');
+console.log(toGreet);
