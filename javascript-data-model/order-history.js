7ac928937be53595aa8602d739a1c60658d71814
Anna Aksenov <annaaksenov8@gmail.com>
2023-03-24 02:40:32 +0000

    Completed js-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,28 @@
+const orderHistory = [
+  {
+    name: 'JavaScript for impatient programmers',
+    description: 'book',
+    delivered: '8/8/2020'
+  },
+  {
+    name: 'The Timeless Way of Building',
+    description: 'book',
+    delivered: '7/20/2020'
+  },
+  {
+    name: 'Gamecube Controller Adapter',
+    description: 'Tech',
+    delivered: '7/7/2020'
+  },
+  {
+    name: 'GameCube Controller',
+    description: 'Tech',
+    delivered: '7/5/2020'
+  },
+  {
+    name: 'The Art of Sql',
+    description: 'book',
+    delivered: '7/5/2020'
+  }
+];
+console.log(orderHistory);
