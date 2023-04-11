37336753861a89c1575fb195c4d946b3f366d630
Anna Aksenov <annaaksenov8@gmail.com>
2023-04-11 04:23:53 +0000

    work in progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,49 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+const players = [
+  {
+    name: 'P1',
+    hand: []
+  },
+  {
+    name: 'P2',
+    hand: []
+  },
+  {
+    name: 'P3',
+    hand: []
+  },
+  {
+    name: 'P4',
+    hand: []
+  }
+];
+const deck = [
+  {
+    rank: '2',
+    suit: 'spades'
+  },
+  {
+    rank: '3',
+    suit: 'spades'
+  },
+  {
+    rank: '4',
+    suit: 'spades'
+  },
+  {
+    rank: '5',
+    suit: 'spades'
+  },
+  {
+    rank: '',
+    suit: ''
+  },
+  {
+    rank: '',
+    suit: ''
+  },
+  {
+    rank: '',
+    suit: ''
+  }
+];
