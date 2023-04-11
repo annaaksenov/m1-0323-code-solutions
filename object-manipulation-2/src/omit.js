1de29814f669e45423eaaa49a154e0b9e31f1bab
Anna Aksenov <annaaksenov8@gmail.com>
2023-04-11 01:54:08 +0000

    Modified omit

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,7 +1,7 @@
 /* exported omit */
 function omit(source, keys) {
-  var res = {};
-  for (var i in source) {
+  const res = {};
+  for (const i in source) {
     if (keys.indexOf(i) >= 0) continue;
     if (!Object.prototype.hasOwnProperty.call(source, i)) continue;
     res[i] = source[i];
