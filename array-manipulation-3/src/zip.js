48f5fdbc10b7ad58d2bf10929096beffe1bdb384
Anna Aksenov <annaaksenov8@gmail.com>
2023-04-14 02:32:30 +0000

    Completed arr-manipulation-3

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,20 +1,5 @@
 /* exported zip */
 function zip(first, second) {
-  const z = [...Array(Math.max(second.length, first.length))].map((_, i) => [first[i], second[i]]);
-
-  console.log(z);
+  const z = [...Array(Math.min(second.length, first.length))].map((_, i) => [first[i], second[i]]);
   return z;
 }
-
-/**
- * const t = first.map(function (e, i) {
-    return [e, second[i]];
-  });
-  console.log(t);
-  return t;
- */
-
-/**
-   * const z = Array.from(Array(Math.max(second.length)), (_, i) => [first[i], second[i]]);
-  return z;
-   */
