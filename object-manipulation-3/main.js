098e83ad4fa73331b9d5da851bcf9cb5848c7a0f
Anna Aksenov <annaaksenov8@gmail.com>
2023-04-26 04:35:12 +0000

    Work

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,5 +1,34 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-const players = [
+
+function game() {
+  const players = [
+    { name: 'Player 1', hand: [] },
+    { name: 'Player 2', hand: [] },
+    { name: 'Player 3', hand: [] },
+    { name: 'Player 4', hand: [] }
+  ];
+  const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+  const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  const deck = [];
+
+  for (var i = 0; i < ranks.length; i++) {
+    for (var j = 0; j < suits.length; j++) {
+      var card = { rank: ranks[i], suit: suits[j] };
+      if (ranks[i] === 'Ace') {
+        card.pointValue = 11;
+      } else if (['King', 'Queen', 'Jack'].includes(ranks[i])) {
+        card.pointValue = 10;
+      } else {
+        card.pointValue = parseInt(ranks[i]);
+      }
+      deck.push(card);
+    }
+  }
+  return players; // delete
+}
+console.log(game);
+
+/** const players = [
   {
     name: 'P1',
     hand: []
@@ -227,9 +256,7 @@ const deck = [
     suit: 'diamonds'
   }
 ];
-const shuff = _.shuffle(deck);
-const deal = _.chunk(shuff, [2]);
-console.log(shuff, deal, players);
+*/
 
 //   function game(players, hand) {
 //  const shuff = _.shuffle(deck);
