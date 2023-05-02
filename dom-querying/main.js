d78e9da12cab019d50344aa4b64bc7bdc1ad5638
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-02 04:02:30 +0000

    Comepleted dom-querying(1)

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,1 +1,18 @@
 console.log('hello, world');
+const header1 = document.querySelector('h1');
+console.log(header1);
+console.dir(header1);
+
+const exp = document.querySelector('#explanation');
+console.log(exp);
+console.dir(exp);
+
+const pclass = document.querySelector('.hint');
+console.log(pclass);
+console.dir(pclass);
+
+const ele = document.querySelectorAll('p');
+console.log(ele);
+
+const class2 = document.querySelectorAll('.example-link');
+console.log(class2);
