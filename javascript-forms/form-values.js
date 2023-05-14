4a8fdd5a870f2bcbec5ef900201f79cfa93a374c
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-14 19:18:00 +0000

    completed javascript-forms

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,5 +2,9 @@ const form = document.querySelector('form');
 
 function logSubmit(event) {
   event.preventDefault();
+  const myFormData = new FormData(event.target);
+  const formDataObj = {};
+  myFormData.forEach((value, key) => (formDataObj[key] = value));
+  console.log(formDataObj);
 }
 form.addEventListener('submit', logSubmit);
