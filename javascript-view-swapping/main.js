a5cd479b0633694ce21c517cb5130a9ef5a7c73b
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-19 04:10:14 +0000

    completed javascript-view-swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,25 @@
+const tabContainer = document.querySelector('.tab-container');
+const tab = document.querySelectorAll('.tab');
+const view = document.querySelectorAll('.view');
+
+function handleClick(event) {
+  if (!event.target.matches('.tab')) {
+    return;
+  }
+  for (let i = 0; i < tab.length; i++) {
+    if (tab[i] === event.target) {
+      tab[i].className = 'tab active';
+      const dataView = event.target.getAttribute('data-view');
+      for (let j = 0; j < view.length; j++) {
+        if (view[j].getAttribute('data-view') === dataView) {
+          view[j].className = 'view';
+        } else {
+          view[j].className = 'view hidden';
+        }
+      }
+    } else {
+      tab[i].className = 'tab';
+    }
+  }
+}
+tabContainer.addEventListener('click', handleClick);
