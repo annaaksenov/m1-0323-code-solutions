5387a35e615807473066753fec08609dd10f495b
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-21 00:52:58 +0000

    almost done

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,20 +2,28 @@
 function titleCase(title) {
   const str = title.toLowerCase().split(' ');
   const exceptions = ['a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'and'];
-  const updatedTitle = str.map(str => {
-    const capitalize = str.charAt(0).toUpperCase() + str.slice(1);
-    if (str === 'javascript') {
-      return 'JavaScript';
-    } else if (str === 'api') {
-      return 'API';
-    } else {
-      return capitalize;
+  const updatedTitle = [];
+  for (let i = 0; i < str.length; i++) {
+    updatedTitle[i] = str[i][0].toUpperCase() + str[i].slice(1).toLowerCase();
+
+    if (str[i] === 'javascript') {
+      updatedTitle[i] = 'JavaScript';
+    } else if (str[i] === 'api') {
+      updatedTitle[i] = 'API';
+    } else if (exceptions.includes(str[i]) && i !== 0) {
+      updatedTitle[i] = str[i].toLowerCase();
+    } else if (str[i] === 'in-depth') {
+      updatedTitle[i] = 'In-Depth';
+    } else if (str[i] === 'self-taught') {
+      updatedTitle[i] = 'Self-Taught';
+    } else if (str[i].includes(':')) {
+      updatedTitle[i] = str[i].toUpperCase() + str[i].slice(i).toLowerCase();
     }
-  });
-  console.log(exceptions);
+  }
   console.log(updatedTitle.join(' '));
   return updatedTitle.join(' ');
 }
+
 /**
  *for (const st in word) {
     updatedTitle[st] = word[st][0].toUpperCase() + word[st].slice(1).toLowerCase();
@@ -31,7 +39,16 @@ function titleCase(title) {
       if (word === 'api') {
         return 'API';
       } else if (word === 'javascript') {
-        return 'JavaScript';
+        returnconst updatedTitle = str.map(str => {
+    const capitalize = str.charAt(0).toUpperCase() + str.slice(1);
+    if (str === 'javascript') {
+      return 'JavaScript';
+    } else if (str === 'api') {
+      return 'API';
+    } else {
+      return capitalize;
+    }
+  }); 'JavaScript';
       } else {
         const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
         return capitalizedWord;
