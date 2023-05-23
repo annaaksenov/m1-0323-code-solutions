2b490766e2ef1209df4bee4a9b9615d977c1b04d
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-23 02:50:45 +0000

    completed javascript-and-json(1)

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -17,12 +17,9 @@ const arr = [
 ];
 console.log(arr[0], arr[1], arr[2]);
 console.log(typeof arr);
-const arr1 = JSON.stringify(arr[0]);
-const arr2 = JSON.stringify(arr[1]);
-const arr3 = JSON.stringify(arr[2]);
-console.log(arr1, typeof arr1);
-console.log(arr2, typeof arr2);
-console.log(arr3, typeof arr3);
+
+const arrays = JSON.stringify(arr);
+console.log(arrays, typeof arrays);
 
 const string = JSON.stringify({ id: '123', name: 'Tate West' });
 console.log(string, typeof string);
