99813dd6730394f2800a8f218d66faf8b132b963
Anna Aksenov <annaaksenov8@gmail.com>
2023-08-01 04:27:25 +0000

    Shuffled, chuncked and dealt cards to player. Next count up points and call winner

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,18 +1,15 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-const players = { Desi: [], Kyle: [], Peter: [], Sofi: [] };
-/* [
+const players = [
   { name: 'Desi', hand: [] },
   { name: 'Kyle', hand: [] },
-  { name: 'Peter', hand: [] },
+  { name: 'Pete', hand: [] },
   { name: 'Sofie', hand: [] }
-]; */
-
-console.log(players[2]);
-
+];
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const suits = ['C', 'D', 'H', 'S'];
+const deck = [];
 function createDeck() {
-  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  const suits = ['C', 'D', 'H', 'S'];
-  const deck = [];
+
   for (let suitCounter = 0; suitCounter < 4; suitCounter++) {
     for (let rankCounter = 0; rankCounter < 13; rankCounter++) {
       deck.push(ranks[rankCounter] + suits[suitCounter]);
@@ -20,24 +17,26 @@ function createDeck() {
   }
   return deck;
 }
-console.log(createDeck());
-//
 
 function shuffleDeck(deck) {
-  return _.shuffle(deck);
+  const shuffChunk = _.chunk(_.shuffle(deck), 2);
+  for (let i = 0; i < players.length; i++) {
+    players[i].hand = [...shuffChunk.splice(0, 1)];
+  }
+  return players;
 }
 console.log(shuffleDeck(createDeck()));
-//
+console.log(players);
 
-/* function deal(eachOne) {
+/*
+ function deal(eachOne) {
   for (let playerCount = 0; playerCount < 4; playerCount++) {
     players[playerCount].hand = shuffleDeck.splice(0, 3);
   }
   return players;
 }
-console.log(deal()); */
-
-/* for (var i = 0; i < ranks.length; i++) {
+console.log(deal());
+ for (var i = 0; i < ranks.length; i++) {
     for (var j = 0; j < suits.length; j++) {
       var card = { rank: ranks[i], suit: suits[j] };
       if (ranks[i] === 'Ace') {
