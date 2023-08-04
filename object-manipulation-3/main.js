1290dc37978406d3c44cf757d8fe48d40366e7f0
Anna Aksenov <annaaksenov8@gmail.com>
2023-08-04 15:57:52 +0000

    Test commit

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -8,8 +8,7 @@ const players = [
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const suits = ['C', 'D', 'H', 'S'];
 const deck = [];
-// const cardValue = { 'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10 };
-// const cardValue = { A: 11, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, J: 10, Q: 10, K: 10 };
+
 function createDeck() {
   for (let suitCounter = 0; suitCounter < 4; suitCounter++) {
     for (let rankCounter = 0; rankCounter < 13; rankCounter++) {
@@ -36,32 +35,42 @@ deal(shuffleDeck(createDeck()), players);
 console.log(deal(shuffleDeck(createDeck()), players));
 
 function callWinner(players) {
-  let score;
-  for (let i = 0; i < players.length; i++) {
-    // const split = players[i].hand.split(' ');
-    if (players[i].hand.includes('A')) {
-      score += 11;
-    }
+  /* const score = 0;
+  const cardValue = ranks.map(({ rank, value }) => ({
+    A: 11,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 10,
+    Q: 10,
+    K: 10
+  }));
+   for (let i = 0; i < players.length; i++) {
+
   }
-  return score;
+  return players; */
 }
 callWinner(players);
 console.log(callWinner(players));
-/*
- for (var i = 0; i < ranks.length; i++) {
-    for (var j = 0; j < suits.length; j++) {
-      var card = { rank: ranks[i], suit: suits[j] };
-      if (ranks[i] === 'Ace') {
-        card.pointValue = 11;
-      } else if (['King', 'Queen', 'Jack'].includes(ranks[i])) {
-        card.pointValue = 10;
-      } else {
-        card.pointValue = parseInt(ranks[i]);
-      }
-      deck.push(card);
-    }
+
+/* const cardValue = ranks.map(n => {
+  let value = 0;
+  if (n === 'A') {
+    value = 11;
+  } else if (n === 'J') {
+    value = 10;
+  } else if (n === 'Q') {
+    value = 10;
+  } else if (n === 'K') {
+    value = 10;
+  } else {
+    value = n;
   }
-  return players; // delete
-}
-console.log(game);
-*/
+  return value;
+}); */
