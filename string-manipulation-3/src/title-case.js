f27db4b799e27cafbe5b7a84faf6017e6a15b98f
Anna Aksenov <annaaksenov8@gmail.com>
2023-08-07 21:42:31 +0000

    Completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -12,10 +12,15 @@ function titleCase(title) {
     str = title.toLowerCase().split('-');
   }
   str = title.toLowerCase().split(' ');
-
   for (let i = 0; i < str.length; i++) {
     updatedTitle[i] = str[i][0].toUpperCase() + str[i].slice(1).toLowerCase();
-
+    if (str[i].includes('-')) {
+      const hyphenSplit = str[i].split('-');
+      const hyphenCapitalized = hyphenSplit
+        .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+        .join('-');
+      updatedTitle[i] = hyphenCapitalized;
+    }
     if (exceptions.includes(str[i]) && i !== 0) {
       updatedTitle[i] = str[i].toLowerCase();
     } else if (str[i] === 'api') {
@@ -25,14 +30,12 @@ function titleCase(title) {
     }
   }
   let joinedTitle = updatedTitle.join(' ');
-
   if (specialChar) {
     joinedTitle =
     joinedTitle.substring(0, specialCharPosition + 2) +
     joinedTitle[specialCharPosition + 2].toUpperCase() +
     joinedTitle.substring(specialCharPosition + 3);
   }
-
   return joinedTitle;
 }
 console.log(titleCase);
